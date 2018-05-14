6b80da9b63e05e53bd41bf29194bc843c38c972d
djoq <danieljoquinn@gmail.com>
2018-05-14 01:28:32 -0400

    add tests

--- test/say_test.js
+++ test/say_test.js
@@ -8,7 +8,7 @@ should = chai.should();
 
 describe('Development environment', function() {
   return it('Prints verbosely', function(done) {
-    say.dev("Successful print");
+    say.dev("Unit tests complete");
     typeof say.dev.should.be.a('Function');
     done();
   });
@@ -18,6 +18,20 @@ describe('Production environment', function() {
   return it('Silenced Output', function(done) {
     say.prod("You should not see this");
     typeof say.prod.should.be.a('Function');
+    done();
+  });
+});
+
+describe('Prints in living color', () => {
+  return it('fails not', (done) => {
+    typeof say.dev('Compatibility tests complete').color.should.be.a('Function');
+    done();
+  });
+});
+
+describe('New color methods are compatible with legacy ', () => {
+  return it('dont break', (done) => {
+    typeof say.prod('anything').color.should.be.a('Function');
     done();
   });
 });
